455caac778d95902bb698e0f2a9300c17ed05b87
angelbinoj <angelvbinoj31@gmail.com>
2025-09-16 18:55:24 +0530

    Updated controllers and error handling

--- controllers/postController.js
+++ controllers/postController.js
@@ -9,23 +9,59 @@ export const getPosts= async(req,res)=>{
 
 //controller to add posts
 export const addPost= async(req,res)=>{
-   const newPost = new Post(req.body);
-   await newPost.save();
-  res.json({message:"new post added", post:newPost});
+    try {
+        const newPost = new Post(req.body);
+        await newPost.save();
+       res.json({success:true,message:"new post added successfulyy", post:newPost});
+        
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            message:"Failed to add posts",
+            error:error.message
+        });
+    }
 }
 
 //controller to delete posts
 export const deletePost=async(req,res)=>{
-    await Post.findByIdAndDelete(req.params.id);
-    res.json({message:"Post deleted successfully"})
+    try {
+        const deletedPost= await Post.findByIdAndDelete(req.params.id);
+        if(!deletedPost){
+            return res.status(404).json({
+                success:false,message:"Post not found"
+            });
+        }
+        res.json({success:true,message:"Post deleted successfully"});
+    } catch (error) {
+         res.status(500).json({
+            success:false,
+            message:"Failed to delete posts",
+            error:error.message
+        });
+    }
 }
 
 //controller to update posts
 export const updatePost=async(req,res)=>{
-    const updatedPost=await Post.findByIdAndUpdate(
-        req.params.id,
-        { $set: req.body },
-    { new: true }
-    );
-    res.json({message:"Post updated successfully",post: updatedPost});
+    try {
+        const updatedPost=await Post.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+        { new: true }
+        );
+         if(!updatedPost){
+                return res.status(404).json({
+                    success:false,message:"Post not found"
+                });
+            }
+        res.json({message:"Post updated successfully",post: updatedPost});
+        
+    } catch (error) {
+          res.status(500).json({
+            success:false,
+            message:"Failed to update posts",
+            error:error.message
+        });
+    }
 }
